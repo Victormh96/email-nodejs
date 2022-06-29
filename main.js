88c27fc72b498c284107f56d4fd4c86fbd76ff72
Victor Hernandez <victorm96hernandez@gmail.com>
2022-06-28 23:31:33 -0600

    'Initial'

---
+++ main.js
@@ -0,0 +1,39 @@
+var nodemailer = require("nodemailer");
+var express = require("express");
+var app = express();
+
+app.post("/send-email", (req, res) => {
+
+    //Servidor SMTP
+    var transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false,
+        auth: {
+            user: "golda.bosco37@ethereal.email",
+            pass: "Ys9fjWBKRhHhYkVhBC",
+        },
+    });
+
+    //Email opciones
+    var mailOptions = {
+        from: "Remitente",
+        to: "victorm96hernandez@gmail.com",
+        subject: "Enviado desde nodemailer",
+        text: "Bienvenido"
+    }
+
+    //Validación
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.status(500).send(error.message)
+        } else {
+            console.log('Enviado');
+            res.status(200).jsonp(req.body)
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor == > http://localhost:3000');
+});
